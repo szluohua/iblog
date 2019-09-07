dc4f7591c8e8ab1a4e390b3c3365bd8492d9c05d
leo <luohuasz@163.com>
2019-09-07 20:58:40 +0800

    添加用户管理

--- components/editUser.vue
+++ components/editUser.vue
@@ -1,41 +1,135 @@
 <template>
-    <div>
-        <a-button type="primary" @click="showModal">Open Modal with async logic</a-button>
-        <a-modal title="Title" :visible="visible" @ok="handleOk" :confirmLoading="confirmLoading" @cancel="handleCancel">
-        </a-modal>
-    </div>
+  <a-form
+    :form="form"
+    @submit="handleSubmit"
+  >
+    <a-form-item
+      v-bind="formItemLayout"
+      label="E-mail"
+    >
+      <a-input
+        v-decorator="[
+          'email',
+          {
+            rules: [{
+              type: 'email', message: 'The input is not valid E-mail!',
+            }, {
+              required: true, message: 'Please input your E-mail!',
+            }]
+          }
+        ]"
+      />
+    </a-form-item>
+    <a-form-item
+      v-bind="formItemLayout"
+      label="Password"
+    >
+      <a-input
+        v-decorator="[
+          'password',
+          {
+            rules: [{
+              required: true, message: 'Please input your password!',
+            }, {
+              validator: validateToNextPassword,
+            }],
+          }
+        ]"
+        type="password"
+      />
+    </a-form-item>
+    <a-form-item
+      v-bind="formItemLayout"
+      label="Confirm Password"
+    >
+      <a-input
+        v-decorator="[
+          'confirm',
+          {
+            rules: [{
+              required: true, message: 'Please confirm your password!',
+            }, {
+              validator: compareToFirstPassword,
+            }],
+          }
+        ]"
+        type="password"
+        @blur="handleConfirmBlur"
+      />
+    </a-form-item>
+
+    <a-form-item v-bind="tailFormItemLayout">
+      <a-button
+        type="primary"
+        html-type="submit"
+      >
+        Register
+      </a-button>
+    </a-form-item>
+  </a-form>
 </template>
 
 <script>
-    export default {
-        props: {
-            userData: {
-                type: Object
-            }
+
+export default {
+  data () {
+    return {
+      confirmDirty: false,
+      formItemLayout: {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 8 },
         },
-        data() {
-            return {
-                ModalText: 'Content of the modal',
-                visible: false,
-                confirmLoading: false,
-            }
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
         },
-        methods: {
-            showModal() {
-                this.visible = true
-            },
-            handleOk(e) {
-                this.ModalText = 'The modal will be closed after two seconds';
-                this.confirmLoading = true;
-                setTimeout(() => {
-                    this.visible = false;
-                    this.confirmLoading = false;
-                }, 2000);
-            },
-            handleCancel(e) {
-                console.log('Clicked cancel button');
-                this.visible = false
-            }
+      },
+      tailFormItemLayout: {
+        wrapperCol: {
+          xs: {
+            span: 24,
+            offset: 0,
+          },
+          sm: {
+            span: 16,
+            offset: 8,
+          },
+        },
+      },
+    };
+  },
+  beforeCreate () {
+    this.form = this.$form.createForm(this);
+  },
+  methods: {
+    handleSubmit  (e) {
+      e.preventDefault();
+      this.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
         }
+      });
+    },
+    handleConfirmBlur  (e) {
+      const value = e.target.value;
+      this.confirmDirty = this.confirmDirty || !!value;
+    },
+    compareToFirstPassword  (rule, value, callback) {
+      const form = this.form;
+      if (value && value !== form.getFieldValue('password')) {
+        callback('Two passwords that you enter is inconsistent!');
+      } else {
+        callback();
+      }
+    },
+    validateToNextPassword  (rule, value, callback) {
+      const form = this.form;
+      if (value && this.confirmDirty) {
+        form.validateFields(['confirm'], { force: true });
+      }
+      callback();
     }
+  },
+};
 </script>
