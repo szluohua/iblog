863001c6d6c5f6c6ff485db9ae763b87fe6b53a9
leo <luohuasz@163.com>
2019-11-14 19:15:41 +0800

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,7 +86,7 @@ module.exports = {
   ** Build configuration
   */
     build: {
-        analyze: true,
+        analyze: false,
         /*
         ** You can extend webpack config here
         */
