e9fdbaa0f32f7b1d7eada2feedca371811675d31
leo <luohuasz@163.com>
2020-05-06 09:46:38 +0800

    添加百度验证meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ module.exports = {
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: pkg.description }
+            { hid: 'description', name: 'description', content: pkg.description },
+            { hid: 'baidu-site-verification', name: 'baidu-site-verification', content: 'ig5NCqvBGx' }
         ],
         link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }].concat(cdnConfig.css),
         script: cdnConfig.js
